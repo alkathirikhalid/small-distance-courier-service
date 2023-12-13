b73dfb8b568c80bb352f64a7d79c29a399c09a49
alkathirikhalid <khalid@alkathirikhalid.com>
2023-12-13 08:48:45 +0800

    Added Class documentation for Coset Discount Use Case and Package Operations Use Case.

--- small-distance-courier-service/src/main/kotlin/usecase/CostDiscountUseCase.kt
+++ small-distance-courier-service/src/main/kotlin/usecase/CostDiscountUseCase.kt
@@ -5,10 +5,26 @@ import domain.cost.TotalCost
 import domain.delivery.Delivery
 import domain.delivery.DeliveryCost
 
+/**
+ * The `CostDiscountUseCase` class provides use case-specific operations related to calculating package costs and discounts.
+ *
+ * This class encapsulates functionality such as calculating delivery costs, package discounts, and total costs for a given delivery.
+ *
+ * @property deliveryCost An instance of the [DeliveryCost] class used for calculating delivery costs.
+ * @property discount An instance of the [Discount] class used for calculating package discounts.
+ * @property totalCost An instance of the [TotalCost] class used for calculating total costs.
+ */
 class CostDiscountUseCase {
     private val deliveryCost = DeliveryCost()
     private val discount = Discount()
     private val totalCost = TotalCost()
+
+    /**
+     * Calculates the delivery cost for each package within a delivery based on the base cost, weight, and distance.
+     *
+     * @param delivery The delivery containing packages to calculate delivery costs for.
+     * @return The modified delivery with updated delivery costs for each package.
+     */
     fun calculatePackageCost(delivery: Delivery): Delivery {
         for (item in delivery.packages) {
             item.deliveryCost = deliveryCost.calculateDeliveryCost(delivery.baseCost, item.weight, item.distance)
@@ -16,6 +32,12 @@ class CostDiscountUseCase {
         return delivery
     }
 
+    /**
+     * Calculates the discount for each package within a delivery based on the package's offer and delivery cost.
+     *
+     * @param delivery The delivery containing packages to calculate discounts for.
+     * @return The modified delivery with updated discounts for each package.
+     */
     fun calculatePackageDiscount(delivery: Delivery): Delivery {
         for (item in delivery.packages) {
             item.discount = discount.calculateDiscount(item.offer, item.deliveryCost)
@@ -23,6 +45,12 @@ class CostDiscountUseCase {
         return delivery
     }
 
+    /**
+     * Calculates the total cost for each package within a delivery based on the delivery cost and discount.
+     *
+     * @param delivery The delivery containing packages to calculate total costs for.
+     * @return The modified delivery with updated total costs for each package.
+     */
     fun calculateTotalCost(delivery: Delivery): Delivery {
         for (item in delivery.packages) {
             item.totalCost = totalCost.calculateTotalCost(item.deliveryCost, item.discount)
--- small-distance-courier-service/src/main/kotlin/usecase/PackageOperationsUseCase.kt
+++ small-distance-courier-service/src/main/kotlin/usecase/PackageOperationsUseCase.kt
@@ -5,12 +5,36 @@ import domain.delivery.Delivery
 import domain.delivery.Package
 import domain.offer.OfferCriteria
 
+/**
+ * The `PackageOperationsUseCase` class provides use case-specific operations related to packages and offers.
+ *
+ * This class encapsulates functionality such as validating package offers and calculating offer percentages
+ * for packages within a delivery.
+ *
+ * @property offerCriteria An instance of the [OfferCriteria] class used for validating package offers.
+ */
 class PackageOperationsUseCase {
     private val offerCriteria = OfferCriteria(Coupons.offers)
+
+    /**
+     * Validates if a given package satisfies the criteria of a predefined discount offer.
+     *
+     * @param item The package to be validated.
+     * @return `true` if the package satisfies the criteria, `false` otherwise.
+     */
     fun validatePackageOffer(item: Package): Boolean {
         return offerCriteria.isValidPackageOffer(item)
     }
 
+    /**
+     * Calculates the offer percentage for each package within a delivery based on predefined discount offers.
+     *
+     * If a package satisfies the criteria of a discount offer, its offer percentage is set accordingly.
+     * If not, the offer percentage is set to 0.0.
+     *
+     * @param delivery The delivery containing packages to calculate offer percentages for.
+     * @return The modified delivery with updated offer percentages for each package.
+     */
     fun getPackageOfferPercentage(delivery: Delivery): Delivery {
         for (item in delivery.packages) {
             if (validatePackageOffer(item)) {
