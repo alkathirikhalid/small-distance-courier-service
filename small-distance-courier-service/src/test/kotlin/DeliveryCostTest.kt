90fdbabe904b8530e807687f8b6d5830a787d328
alkathirikhalid <khalid@alkathirikhalid.com>
2023-12-13 15:15:49 +0800

    Refactor Code

---
+++ small-distance-courier-service/src/test/kotlin/DeliveryCostTest.kt
@@ -0,0 +1,169 @@
+import domain.cost.Discount
+import domain.cost.TotalCost
+import domain.delivery.Delivery
+import domain.delivery.DeliveryCost
+import domain.delivery.Package
+import org.junit.jupiter.api.Test
+import usecase.CostDiscountUseCase
+import kotlin.test.assertEquals
+
+class DeliveryCostTest {
+    @Test
+    fun `Delivery Cost Calculation`() {
+        // Delivery Cost = Base Delivery Cost + (Package Total Weight * 10) + (Distance to Destination * 5)
+
+        // Arrange
+        val deliveryCost = DeliveryCost()
+        val costDiscountUseCase = CostDiscountUseCase()
+
+        val baseCost = 100.0
+        val packageTotalWeight = 30.0
+        val distanceToDestination = 50.0
+
+        val delivery = Delivery()
+        val item = Package("PKG1", packageTotalWeight, distanceToDestination, "", 0.0, 0.0, 0.0, 0.0, 0.0)
+        delivery.packages.add(item)
+        delivery.baseCost = baseCost
+
+        // Act
+        val result1 = deliveryCost.calculateDeliveryCost(baseCost, packageTotalWeight, distanceToDestination)
+        val result2 = costDiscountUseCase.calculatePackageCost(delivery)
+
+        // Assert
+        assertEquals(650.0, result1)
+        assertEquals(650.0, result2.packages[0].deliveryCost)
+    }
+
+    @Test
+    fun `Discount Calculation`() {
+        // Arrange
+        val costDiscountUseCase = CostDiscountUseCase()
+        val delivery = Delivery()
+
+        val item10 = Package("", 0.0, 0.0, "", 10.0, 50.0, 0.0, 0.0, 0.0)
+        val item07 = Package("", 0.0, 0.0, "", 7.0, 50.0, 0.0, 0.0, 0.0)
+        val item05 = Package("", 0.0, 0.0, "", 5.0, 50.0, 0.0, 0.0, 0.0)
+        val item00 = Package("", 0.0, 0.0, "", 0.0, 50.0, 0.0, 0.0, 0.0)
+
+        delivery.packages.add(item10)
+        delivery.packages.add(item07)
+        delivery.packages.add(item05)
+        delivery.packages.add(item00)
+
+        // Act
+        costDiscountUseCase.calculatePackageDiscount(delivery)
+
+        // Assert
+        assertEquals(5.0, delivery.packages[0].discount)
+        assertEquals(3.5000000000000004, delivery.packages[1].discount)
+        assertEquals(2.5, delivery.packages[2].discount)
+        assertEquals(0.0, delivery.packages[3].discount)
+    }
+
+
+    @Test
+    fun `Discount Calculation 0 Percent`() {
+        // Discount = (Offer / 100) * Delivery Cost.
+
+        // Arrange
+        val discount = Discount()
+        val packageOffer = 0.0
+        val packageCost = 50.0
+
+        // Act
+        val result = discount.calculateDiscount(packageOffer, packageCost)
+
+        // Assert
+        assertEquals(0.0, result)
+    }
+
+    @Test
+    fun `Discount Calculation 5 Percent`() {
+        // Discount = (Offer / 100) * Delivery Cost.
+
+        // Arrange
+        val discount = Discount()
+        val packageOffer = 5.0
+        val packageCost = 50.0
+
+        // Act
+        val result = discount.calculateDiscount(packageOffer, packageCost)
+
+        // Assert
+        assertEquals(2.5, result)
+    }
+
+    @Test
+    fun `Discount Calculation 7 Percent`() {
+        // Discount = (Offer / 100) * Delivery Cost.
+
+        // Arrange
+        val discount = Discount()
+        val packageOffer = 7.0
+        val packageCost = 50.0
+
+        // Act
+        val result = discount.calculateDiscount(packageOffer, packageCost)
+
+        // Assert
+        assertEquals(3.5000000000000004, result)
+    }
+
+    @Test
+    fun `Discount Calculation 10 Percent`() {
+        // Discount = (Offer / 100) * Delivery Cost.
+
+        // Arrange
+        val discount = Discount()
+        val packageOffer = 10.0
+        val packageCost = 50.0
+
+        // Act
+        val result = discount.calculateDiscount(packageOffer, packageCost)
+
+        // Assert
+        assertEquals(5.0, result)
+    }
+
+    @Test
+    fun `Total Cost Calculation`() {
+        // Arrange
+        val costDiscountUseCase = CostDiscountUseCase()
+        val delivery = Delivery()
+
+        val item10 = Package("", 0.0, 0.0, "", 0.0, 50.0, 5.0, 0.0, 0.0)
+        val item07 = Package("", 0.0, 0.0, "", 0.0, 50.0, 3.5, 0.0, 0.0)
+        val item05 = Package("", 0.0, 0.0, "", 0.0, 50.0, 2.5, 0.0, 0.0)
+        val item00 = Package("", 0.0, 0.0, "", 0.0, 50.0, 0.0, 0.0, 0.0)
+
+        delivery.packages.add(item10)
+        delivery.packages.add(item07)
+        delivery.packages.add(item05)
+        delivery.packages.add(item00)
+
+        // Act
+        costDiscountUseCase.calculateTotalCost(delivery)
+
+        // Assert
+        assertEquals(45.0, delivery.packages[0].totalCost)
+        assertEquals(46.5, delivery.packages[1].totalCost)
+        assertEquals(47.5, delivery.packages[2].totalCost)
+        assertEquals(50.0, delivery.packages[3].totalCost)
+    }
+
+    @Test
+    fun `Total Cost`() {
+        // Discount = (Offer / 100) * Delivery Cost.
+
+        // Arrange
+        val totalCostCalculator = TotalCost()
+
+        // Act
+        val packageCost = 100.0
+        val packageDiscount = 20.0
+        val totalCost = totalCostCalculator.calculateTotalCost(packageCost, packageDiscount)
+
+        // Assert
+        assertEquals(80.0, totalCost, "Total cost calculation is incorrect")
+    }
+}
--- small-distance-courier-service/src/test/kotlin/DeliveryTimeTest.kt
+++ small-distance-courier-service/src/test/kotlin/DeliveryTimeTest.kt
@@ -1,176 +1,13 @@
-import domain.cost.Discount
-import domain.cost.TotalCost
 import domain.delivery.Delivery
-import domain.delivery.DeliveryCost
 import domain.delivery.DeliveryCriteria
 import domain.delivery.Package
 import domain.transportation.Vehicle
 import org.junit.jupiter.api.Test
-import usecase.CostDiscountUseCase
 import usecase.DeliveryTimeUseCase
 import usecase.RemainderPackageDeliveryTimeUseCase
 import kotlin.test.assertEquals
 
-class DeliveryTest {
-    @Test
-    fun `Delivery Cost Calculation`() {
-        // Delivery Cost = Base Delivery Cost + (Package Total Weight * 10) + (Distance to Destination * 5)
-
-        // Arrange
-        val deliveryCost = DeliveryCost()
-        val costDiscountUseCase = CostDiscountUseCase()
-
-        val baseCost = 100.0
-        val packageTotalWeight = 30.0
-        val distanceToDestination = 50.0
-
-        val delivery = Delivery()
-        val item = Package("PKG1", packageTotalWeight, distanceToDestination, "", 0.0, 0.0, 0.0, 0.0, 0.0)
-        delivery.packages.add(item)
-        delivery.baseCost = baseCost
-
-        // Act
-        val result1 = deliveryCost.calculateDeliveryCost(baseCost, packageTotalWeight, distanceToDestination)
-        val result2 = costDiscountUseCase.calculatePackageCost(delivery)
-
-        // Assert
-        assertEquals(650.0, result1)
-        assertEquals(650.0, result2.packages[0].deliveryCost)
-    }
-
-    @Test
-    fun `Discount Calculation`() {
-        // Arrange
-        val costDiscountUseCase = CostDiscountUseCase()
-        val delivery = Delivery()
-
-        val item10 = Package("", 0.0, 0.0, "", 10.0, 50.0, 0.0, 0.0, 0.0)
-        val item07 = Package("", 0.0, 0.0, "", 7.0, 50.0, 0.0, 0.0, 0.0)
-        val item05 = Package("", 0.0, 0.0, "", 5.0, 50.0, 0.0, 0.0, 0.0)
-        val item00 = Package("", 0.0, 0.0, "", 0.0, 50.0, 0.0, 0.0, 0.0)
-
-        delivery.packages.add(item10)
-        delivery.packages.add(item07)
-        delivery.packages.add(item05)
-        delivery.packages.add(item00)
-
-        // Act
-        costDiscountUseCase.calculatePackageDiscount(delivery)
-
-        // Assert
-        assertEquals(5.0, delivery.packages[0].discount)
-        assertEquals(3.5000000000000004, delivery.packages[1].discount)
-        assertEquals(2.5, delivery.packages[2].discount)
-        assertEquals(0.0, delivery.packages[3].discount)
-    }
-
-
-    @Test
-    fun `Discount Calculation 0 Percent`() {
-        // Discount = (Offer / 100) * Delivery Cost.
-
-        // Arrange
-        val discount = Discount()
-        val packageOffer = 0.0
-        val packageCost = 50.0
-
-        // Act
-        val result = discount.calculateDiscount(packageOffer, packageCost)
-
-        // Assert
-        assertEquals(0.0, result)
-    }
-
-    @Test
-    fun `Discount Calculation 5 Percent`() {
-        // Discount = (Offer / 100) * Delivery Cost.
-
-        // Arrange
-        val discount = Discount()
-        val packageOffer = 5.0
-        val packageCost = 50.0
-
-        // Act
-        val result = discount.calculateDiscount(packageOffer, packageCost)
-
-        // Assert
-        assertEquals(2.5, result)
-    }
-
-    @Test
-    fun `Discount Calculation 7 Percent`() {
-        // Discount = (Offer / 100) * Delivery Cost.
-
-        // Arrange
-        val discount = Discount()
-        val packageOffer = 7.0
-        val packageCost = 50.0
-
-        // Act
-        val result = discount.calculateDiscount(packageOffer, packageCost)
-
-        // Assert
-        assertEquals(3.5000000000000004, result)
-    }
-
-    @Test
-    fun `Discount Calculation 10 Percent`() {
-        // Discount = (Offer / 100) * Delivery Cost.
-
-        // Arrange
-        val discount = Discount()
-        val packageOffer = 10.0
-        val packageCost = 50.0
-
-        // Act
-        val result = discount.calculateDiscount(packageOffer, packageCost)
-
-        // Assert
-        assertEquals(5.0, result)
-    }
-
-    @Test
-    fun `Total Cost Calculation`() {
-        // Arrange
-        val costDiscountUseCase = CostDiscountUseCase()
-        val delivery = Delivery()
-
-        val item10 = Package("", 0.0, 0.0, "", 0.0, 50.0, 5.0, 0.0, 0.0)
-        val item07 = Package("", 0.0, 0.0, "", 0.0, 50.0, 3.5, 0.0, 0.0)
-        val item05 = Package("", 0.0, 0.0, "", 0.0, 50.0, 2.5, 0.0, 0.0)
-        val item00 = Package("", 0.0, 0.0, "", 0.0, 50.0, 0.0, 0.0, 0.0)
-
-        delivery.packages.add(item10)
-        delivery.packages.add(item07)
-        delivery.packages.add(item05)
-        delivery.packages.add(item00)
-
-        // Act
-        costDiscountUseCase.calculateTotalCost(delivery)
-
-        // Assert
-        assertEquals(45.0, delivery.packages[0].totalCost)
-        assertEquals(46.5, delivery.packages[1].totalCost)
-        assertEquals(47.5, delivery.packages[2].totalCost)
-        assertEquals(50.0, delivery.packages[3].totalCost)
-
-    }
-
-    @Test
-    fun `Total Cost`() {
-        // Discount = (Offer / 100) * Delivery Cost.
-
-        // Arrange
-        val totalCostCalculator = TotalCost()
-
-        // Act
-        val packageCost = 100.0
-        val packageDiscount = 20.0
-        val totalCost = totalCostCalculator.calculateTotalCost(packageCost, packageDiscount)
-
-        // Assert
-        assertEquals(80.0, totalCost, "Total cost calculation is incorrect")
-    }
+class DeliveryTimeTest {
 
     @Test
     fun `Each Vehicle has a limit (L) on maximum weight (kg) that it can carry`() {
@@ -238,29 +75,19 @@ class DeliveryTest {
 
         // Assert
         assertEquals(
-            0.42,
-            delivery.packages[0].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            0.42, delivery.packages[0].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            1.78,
-            delivery.packages[1].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            1.78, delivery.packages[1].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            1.42,
-            delivery.packages[2].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            1.42, delivery.packages[2].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            0.85,
-            delivery.packages[3].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            0.85, delivery.packages[3].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            1.35,
-            delivery.packages[4].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            1.35, delivery.packages[4].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
     }
 
@@ -276,14 +103,10 @@ class DeliveryTest {
 
         // Assert
         assertEquals(
-            2,
-            delivery.vehicles[0].packagesToDeliver.size,
-            "Expected PKG to delivery is incorrect"
+            2, delivery.vehicles[0].packagesToDeliver.size, "Expected PKG to delivery is incorrect"
         )
         assertEquals(
-            1,
-            delivery.vehicles[1].packagesToDeliver.size,
-            "Expected PKG to delivery is incorrect"
+            1, delivery.vehicles[1].packagesToDeliver.size, "Expected PKG to delivery is incorrect"
         )
     }
 
@@ -300,14 +123,10 @@ class DeliveryTest {
 
         // Assert
         assertEquals(
-            3.56,
-            delivery.vehicles[0].availableTime,
-            "Expected time to be available is incorrect"
+            3.56, delivery.vehicles[0].availableTime, "Expected time to be available is incorrect"
         )
         assertEquals(
-            2.84,
-            delivery.vehicles[1].availableTime,
-            "Expected time to be available is incorrect"
+            2.84, delivery.vehicles[1].availableTime, "Expected time to be available is incorrect"
         )
     }
 
@@ -326,29 +145,19 @@ class DeliveryTest {
 
         // Assert
         assertEquals(
-            3.98,
-            delivery.packages[0].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            3.98, delivery.packages[0].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            1.78,
-            delivery.packages[1].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            1.78, delivery.packages[1].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            1.42,
-            delivery.packages[2].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            1.42, delivery.packages[2].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            0.85,
-            delivery.packages[3].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            0.85, delivery.packages[3].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
         assertEquals(
-            4.1899999999999995,
-            delivery.packages[4].estimatedDeliveryTime,
-            "Expected PKG delivery time is incorrect"
+            4.1899999999999995, delivery.packages[4].estimatedDeliveryTime, "Expected PKG delivery time is incorrect"
         )
     }
 
